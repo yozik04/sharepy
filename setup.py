416976bb51bd3fc5554e674241f4e37d7bdb67fa
jevgenik <jevgenik@microsoft.com>
2017-11-29 16:10:47 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name='sharepy',
+  version='1.2.0',
+  packages=[''],
+  url='https://github.com/yozik04/sharepy.git',
+  license='GPLv3',
+  author='JonathanHolvey',
+  author_email='jonathan.holvey@outlook.com',
+  description='Simple SharePoint Online authentication for Python'
+)
